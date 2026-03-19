901eb917d02031aeb81c769e91fe2d57881bae69
Monaka <zengjiawei5tc@gmail.com>
2026-03-19 13:11:59 +0800

    修改编译配置

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,18 @@ plugins {
 }
 
 android {
+    signingConfigs {
+        create("release") {
+            storeFile = file("E:\\workspace\\flutter\\sign.jks")
+            storePassword = "ww20041101"
+            keyAlias = "sign"
+            keyPassword = "ww20041101"
+        }
+    }
+
     namespace = "com.example.schulte_grid"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -25,7 +34,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
